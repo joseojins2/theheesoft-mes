e6eb43ae90b1585c95d9f0c49f12b7059ea571eb
joseojin <tjwls8561@naver.com>
2026-08-05 00:44:52 +0900

    update dashboard layout

--- src/components/dashboard/ModuleGrid.tsx
+++ src/components/dashboard/ModuleGrid.tsx
@@ -128,8 +128,8 @@ export default function ModuleGrid(){
       grid
       grid-cols-1
       sm:grid-cols-2
-      xl:grid-cols-4
-      2xl:grid-cols-8
+      lg:grid-cols-4
+      xl:grid-cols-8
       gap-3
       w-full
       "
